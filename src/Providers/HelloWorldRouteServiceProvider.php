cb458edb3047968bfd2a9039b3aac67fa761b67a
dark-cms <info@dark-cms.de>
2017-09-06 12:03:29 +0200

    Update HelloWorldRouteServiceProvider.php

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -15,7 +15,6 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router)
 	{
-		var_dump($router);
 		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
 	}
 
